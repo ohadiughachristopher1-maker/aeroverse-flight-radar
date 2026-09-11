db60a53a3757d61903a9bd9708286636417663d4
Ohadiugha Christopher Chidera <ohadiugha@gmail.com>
2026-09-11 16:10:11 +0100

    Reduce OpenSky connection timeout

--- app.py
+++ app.py
@@ -109,7 +109,7 @@ def get_token():
                     "client_id": CLIENT_ID,
                     "client_secret": CLIENT_SECRET
                 },
-                timeout=60
+                timeout=15
             )
             response.raise_for_status()
             break
